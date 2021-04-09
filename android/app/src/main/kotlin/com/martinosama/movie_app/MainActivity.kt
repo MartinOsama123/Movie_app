52a09b6ed6b713b677164c817a1149670d60769e
Martin Osama <martinosama2015@gmail.com>
2021-04-09 16:06:05 +0200

    Added packages,Movie Model & json responds in Data file

---
+++ android/app/src/main/kotlin/com/martinosama/movie_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.martinosama.movie_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Data.dart
@@ -0,0 +1,55 @@
+
+import 'dart:convert';
+
+import 'package:movie_app/Model/MovieModel.dart';
+import 'package:http/http.dart' as http;
+class Data {
+  static const IP_ADDRESS = "https://api.themoviedb.org/3/movie/";
+  static const API = "api_key=3341631117f8da98766219dfd5703269";
+
+  static Future<MovieModel> getNowPlaying({int page = 1}) async {
+    MovieModel movieModel;
+
+    var response = await http.get(
+        Uri.parse("$IP_ADDRESS/now_playing?$API&page=$page"));
+    if (response != null && response.statusCode == 200) {
+      try {
+        movieModel = MovieModel.fromJson(jsonDecode(response.body));
+      } catch (e) {
+        print(e.toString());
+      }
+    }
+    print("Finsihed");
+    return movieModel;
+  }
+  static Future<MovieModel> getTopRated({int page = 1}) async {
+    MovieModel movieModel;
+
+    var response = await http.get(
+        Uri.parse("$IP_ADDRESS/top_rated?$API&page=$page"));
+    if (response != null && response.statusCode == 200) {
+      try {
+        movieModel = MovieModel.fromJson(jsonDecode(response.body));
+      } catch (e) {
+        print(e.toString());
+      }
+    }
+    print("Finsihed");
+    return movieModel;
+  }
+  static Future<MovieModel> searchMovies({String query="",int page = 1}) async {
+    MovieModel movieModel;
+
+    var response = await http.get(
+        Uri.parse("$IP_ADDRESS/search/movie?query=$query&$API&page=$page"));
+    if (response != null && response.statusCode == 200) {
+      try {
+        movieModel = MovieModel.fromJson(jsonDecode(response.body));
+      } catch (e) {
+        print(e.toString());
+      }
+    }
+    print("Finsihed");
+    return movieModel;
+  }
+}
